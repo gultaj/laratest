3881b16de8a821f4a50fc56a2331f39448f37966
gultaj <yaschick.mail@gmail.com>
2015-04-13 10:33:09 +0300

    add create form

--- app/views/admin/posts/create.blade.php
+++ app/views/admin/posts/create.blade.php
@@ -1,1 +1,13 @@
-D:\Doc\Projects\laratest\app/views/posts/create.blade.php
+@extends('admin._layouts.admin')
+
+@section('content')
+
+  <h1>Create post</h1>
+
+  {{ Form::open(['route' => 'admin.posts.store']) }}
+
+    @include('admin.posts._partials.form')
+
+  {{ Form::close() }}
+
+@stop
--- app/views/admin/posts/index.blade.php
+++ app/views/admin/posts/index.blade.php
@@ -2,11 +2,19 @@
 
 @section('content')
 
+  <h1>Posts</h1>
+  {{ link_to_route('admin.posts.create', 'Create new post') }}
 
   @if(count($posts))
     <ul>
     @foreach ($posts as $post)
-      <li>{{ link_to_route('admin.posts.edit', $post->title, [$post->id]) }}</li>
+      <li>
+        {{ link_to_route('admin.posts.edit', $post->title, [$post->id]) }}
+        {{ Form::open(['route' => ['admin.posts.destroy', $post->id], 'method' => 'DELETE', 'class' => 'destroy']) }}
+          {{ Form::submit('Delete') }}
+        {{ Form::close() }}
+      </li>
+
     @endforeach
     </ul>
   @endif
